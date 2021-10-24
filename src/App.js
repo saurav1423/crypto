7a05de6ad3e97aabc417d64d7a1dfabbffb39827
Serry Saurav <sauravkumar080999@gmail.com>
2021-10-25 00:08:55 +0530

    sit 1.1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
 import { Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>
