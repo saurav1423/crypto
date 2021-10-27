ca9d2de1450a5042cada107a4b98792feec72a91
Serry Saurav <sauravkumar080999@gmail.com>
2021-10-27 19:54:31 +0530

    layout done

---
+++ src/components/Cryptocurrencies.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cryptocurrencies = () => {
+  return <div>cryptocurrencies</div>;
+};
+
+export default Cryptocurrencies;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
+import "antd/dist/antd.css";
+
 ReactDOM.render(
   <Router>
     <App />
